4198b3824e0dd08d69a2c880741812b3c093dcfe
HitmanActual <dev.render@gmail.com>
2020-05-18 01:53:05 +0200

    deleted at and handler

--- database/migrations/2020_05_06_123901_create_tags_table.php
+++ database/migrations/2020_05_06_123901_create_tags_table.php
@@ -18,6 +18,7 @@ class CreateTagsTable extends Migration
             $table->string('tag_name')->nullable();
             $table->integer('category_id')->unsigned();
             $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
